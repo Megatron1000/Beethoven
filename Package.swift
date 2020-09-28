1b4af0b4ab8b2847a584a29af26cec456118c64c
Mark Bridges <mark@bridgetech.io>
2020-09-28 17:11:12 +0100

    Fixed package not resolving issue

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-  name: "Beethoven",
+    name: "Beethoven",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         .library(name: "Beethoven", targets: ["Beethoven"]),
     ],
-  dependencies: [
-    .package(url: "https://github.com/Megatron1000/Pitchy",.upToNextMajor(from: Version(stringLiteral: "3")))
-  ],
+    dependencies: [
+        .package(url: "https://github.com/Megatron1000/Pitchy", from: "3.0.0")
+    ],
     targets: [
         .target(name: "Beethoven", dependencies: [], path: "Source"),
         .testTarget(name: "Beethoven-iOS-Tests", dependencies: [], path: "Tests"),
